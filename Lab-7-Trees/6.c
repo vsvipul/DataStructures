bda1d90a8c58307bd8b492082ddaae90490f7e84
ViPuL <vsvipul555@gmail.com>
2017-04-28 15:22:40 +0530

    Create 6.c

---
+++ Lab-7-Trees/6.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node
+{
+	struct node *lchild;
+	int info;
+	struct node *rchild;
+};
+
+struct node* insert(struct node* ptr, int key)
+{
+	if (ptr==NULL)
+	{
+		ptr=(struct node*)malloc(sizeof(struct node));
+		ptr->info=key;
+		ptr->lchild=NULL;
+		ptr->rchild=NULL;
+	}
+	else if (key<ptr->info)
+		ptr->lchild=insert(ptr->lchild,key);
+	else if (key>ptr->info)
+		ptr->rchild=insert(ptr->rchild,key);
+	else
+		printf("Duplicate key\n");
+	return ptr;
+}
+
+int sumleaves(struct node* p)
+{
+	if (p==NULL)
+		return 0;
+	if (p->lchild==NULL && p->rchild==NULL)
+		return p->info;
+	else
+		return sumleaves(p->lchild)+sumleaves(p->rchild);
+}
+
+int main()
+{
+	struct node *root=NULL;
+	int n,i,tmp;
+	scanf("%d",&n);
+	for (i=0;i<n;i++)
+	{
+		scanf("%d",&tmp);
+		root=insert(root,tmp);
+	}
+	int leavesum=sumleaves(root);
+	printf("%d\n",leavesum);
+	return 0;
+}
